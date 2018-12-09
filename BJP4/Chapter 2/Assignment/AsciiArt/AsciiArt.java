088cd987e5bdb68178a4cffc4bad389d7ebd0251
edsonmendieta <emendietaj@gmail.com>
2018-12-09 09:59:48 -0800

    added size 4 needle tip.

--- BJP4/Chapter 2/Assignment/AsciiArt/AsciiArt.java
+++ BJP4/Chapter 2/Assignment/AsciiArt/AsciiArt.java
@@ -1,13 +1,21 @@
 public class AsciiArt {
    
    public static void main(String[] args) {
+      tip();
       upperLid();
    }
    
    
    public static void tip() {
       for (int i = 1; i <= 4; i++) {
-         System.out.println("| |");
+         for (int j = 1; j <= 6 * (4 - 1) + 6; j++) {
+            System.out.print(" ");
+         }//Left-side-spaces
+         System.out.print(" | | ");
+         for (int j = 1; j <= 6 * (4 - 1) + 6; j++) {
+            System.out.print(" ");
+         }//Right-side-spaces
+         System.out.println();
       }
    }//end-of-tip()
    
