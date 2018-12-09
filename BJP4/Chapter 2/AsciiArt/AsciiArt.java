6b2b4a8a927a0200e749d70f631a4e0268ef5a57
edsonmendieta <emendietaj@gmail.com>
2018-12-09 13:05:08 -0800

    scalable space needle

--- BJP4/Chapter 2/AsciiArt/AsciiArt.java
+++ BJP4/Chapter 2/AsciiArt/AsciiArt.java
@@ -1,4 +1,6 @@
 public class AsciiArt {
+
+   public static final int SIZE = 7;
    
    public static void main(String[] args) {
       tip();
@@ -13,21 +15,18 @@ public class AsciiArt {
    
    
    public static void tip() {
-      for (int i = 1; i <= 4; i++) {
-         for (int j = 1; j <= 6 * (4 - 1) + 6; j++) {
+      for (int i = 1; i <= SIZE; i++) {
+         for (int j = 1; j <= 6 * (SIZE - 1) + 6; j++) {
             System.out.print(" ");
          }//Left-side-spaces
          System.out.print(" | | ");
-         for (int j = 1; j <= 6 * (4 - 1) + 6; j++) {
-            System.out.print(" ");
-         }//Right-side-spaces
          System.out.println();
-      }
+      }//outermost-line-loop
    }//end-of-tip()
    
    public static void lidTop() {
-      for (int i = 1; i <= 4; i++) {
-         for (int j = 1; j <= 6 * (4 - i); j++) {
+      for (int i = 1; i <= SIZE; i++) {
+         for (int j = 1; j <= 6 * (SIZE - i); j++) {
             System.out.print(" ");
          }//Left-side-spaces 
          System.out.print("_____");
@@ -41,16 +40,13 @@ public class AsciiArt {
          }//Right-side-colons
          System.out.print("\\");
          System.out.print("_____");
-          for (int j = 1; j <= 6 * (4 - i); j++) {
-            System.out.print(" ");
-         }//Right-side-spaces
          System.out.println();
       }//outer-most-line-loop
    }//end-of-lidTop()
    
    public static void lidQuotes() {
       System.out.print(" | ");
-      for (int i = 1; i <= 3 * (4 - 1) * 2 + 6; i++) {
+      for (int i = 1; i <= 3 * (SIZE - 1) * 2 + 6; i++) {
          System.out.print("\" ");
       }
       System.out.print("| ");
@@ -58,12 +54,12 @@ public class AsciiArt {
    }//end-of-lidQuotes()
    
    public static void lidBottom() {
-      for (int i = 1; i <= 4; i++) {
+      for (int i = 1; i <= SIZE; i++) {
          for (int k = 1; k <= (i - 1) * 4; k++) {
             System.out.print(" ");
          }//initial-spaces
          System.out.print(" \\___/ ");
-         for (int j = 1; j <= i * -2 + 12; j++) {
+         for (int j = 1; j <= i * -2 + SIZE * 3; j++) {
             System.out.print("\\ / ");
          }//inner-slash-pairs
          System.out.print("\\___/");
@@ -72,8 +68,8 @@ public class AsciiArt {
    }//end-of-lidBottom()
    
    public static void midSection() {
-      for (int i = 1; i <= 4 * 4; i++) {
-         for (int j = 1; j <= 6 * (4 - 1) + 4; j++) {
+      for (int i = 1; i <= SIZE * SIZE; i++) {
+         for (int j = 1; j <= 6 * (SIZE - 1) + 4; j++) {
             System.out.print(" ");
          }//Left-side-spaces
          System.out.print("|%%| |%%|");
