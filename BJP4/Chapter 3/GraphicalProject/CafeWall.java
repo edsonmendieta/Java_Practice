0cf6c45990c141f762205c15207d96a5fd854747
edsonmendieta <emendietaj@gmail.com>
2018-12-11 16:22:48 -0800

    set up files

---
+++ BJP4/Chapter 3/GraphicalProject/CafeWall.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class CafeWall {
+
+   public static void main(String[] args) {
+   
+      DrawingPanel canvas = new DrawingPanel (650, 400);
+      canvas.setBackground(Color.GRAY);
+   
+   }
+   
+}// end-of-program
