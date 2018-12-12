2821ac8b3cd89ef69081538c6054b7830afc81f0
edsonmendieta <emendietaj@gmail.com>
2018-12-11 17:09:43 -0800

    added drawPairMethod.

--- BJP4/Chapter 3/GraphicalProject/CafeWall.java
+++ BJP4/Chapter 3/GraphicalProject/CafeWall.java
@@ -12,14 +12,24 @@ public class CafeWall {
    }
    
    
-   public static void drawRow(Graphics g, int x, int y, int boxPairs, int boxSize) {
+   public static void drawPair(Graphics g, int x, int y, int boxSize) {
    
       g.setColor(Color.BLACK);
       g.fillRect(x, y, boxSize, boxSize);
       g.setColor(Color.WHITE);
       g.drawLine(x, y, x + boxSize, y + boxSize);
       g.drawLine(x, y + boxSize, x + boxSize, y);
+      
+      g.setColor(Color.WHITE);
+      g.fillRect(x + boxSize, y, boxSize, boxSize);
+
+   }// drawPair()
    
-   }
+   
+   public static void drawRow(Graphics g, int x, int y, int boxPairs, int boxSize) {
+   
+      drawPair(g, x, y, boxSize);
+      
+   }// drawRow()
    
 }// end-of-program
