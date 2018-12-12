fe8db333cc09a8a3614f4b101f274de81ac2beaf
edsonmendieta <emendietaj@gmail.com>
2018-12-11 16:59:17 -0800

    added box method

--- BJP4/Chapter 3/GraphicalProject/CafeWall.java
+++ BJP4/Chapter 3/GraphicalProject/CafeWall.java
@@ -6,6 +6,19 @@ public class CafeWall {
    
       DrawingPanel canvas = new DrawingPanel (650, 400);
       canvas.setBackground(Color.GRAY);
+      Graphics g = canvas.getGraphics();
+      
+      drawRow(g, 0, 0, 1, 20);
+   }
+   
+   
+   public static void drawRow(Graphics g, int x, int y, int boxPairs, int boxSize) {
+   
+      g.setColor(Color.BLACK);
+      g.fillRect(x, y, boxSize, boxSize);
+      g.setColor(Color.WHITE);
+      g.drawLine(x, y, x + boxSize, y + boxSize);
+      g.drawLine(x, y + boxSize, x + boxSize, y);
    
    }
    
