38f2c01db225c9e877c62aa07bb26cac78cfd5e4
edsonmendieta <emendietaj@gmail.com>
2018-12-22 10:53:20 -0800

    added solution.

---
+++ A_Java_Book/Chapter_3/PrintPowersOfN.java
@@ -0,0 +1,17 @@
+public class PrintPowersOfN {
+   public class void main(String[] args) {
+   printPowersofN1(4, 3);
+   printPowersofN1(-6, 0);
+   printPowersofN1(-2, 8);   
+   }
+   
+   printPowersofN1(int base, int exponent){
+      System.out.print("1 ");
+      int total = 1;
+      for (int i = 1; i <= exponent; i++) { // running product total * base; done 'exponent' amount of times
+         total = (total * base);
+         System.out.print(total + " ");
+      }
+   }
+  
+}
