1f39f07d6bffc5e3746fbc45e5fbfea674ce0c71
edsonmendieta <emendietaj@gmail.com>
2018-12-25 20:15:08 -0800

    added practice exercise 'PrintGrid'.

---
+++ A_Java_Book/Chapter_3/PrintGrid.java
@@ -0,0 +1,17 @@
+public class PrintGrid {
+
+   public static void main(String[] args) {
+   
+   }
+   
+   
+   public static void printGrid1(int rows, int cols) {
+      
+      for (int i = 1; i <= rows; i++) {
+         for (int j = 1; j <= cols; j++) {
+         
+         }
+      }
+   } // printGrid1()
+
+}
