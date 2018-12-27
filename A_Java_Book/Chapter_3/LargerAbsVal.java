75ccdbb899ce78e7c18dc944d502c2a32499aa45
edsonmendieta <emendietaj@gmail.com>
2018-12-26 23:18:21 -0800

    completed LargerAbsVal solution.

--- A_Java_Book/Chapter_3/LargerAbsVal.java
+++ A_Java_Book/Chapter_3/LargerAbsVal.java
@@ -1,12 +1,20 @@
-public class LargerAbsVal {
+public class LargerAbsVal { // Program takes two user inputed integers and
+// returns largest absolute value.
 
    public static void main(String[] args) {
-   
+      int test1 = largerAbsolute(11, 2);
+      System.out.print(test1);
+      
+      System.out.println();
+      
+      int test2 = largerAbsolute(4, -5);
+      System.out.print(test2);
    }
    
    
    public static int largerAbsolute(int num1, int num2) {
-      
+     
+      return Math.max(Math.abs(num1), Math.abs(num2)); // returns largest absolute
    }
 
 }
