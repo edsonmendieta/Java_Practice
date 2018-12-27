b9895cbab13ccefe7cc60652737a142ae34cae9b
edsonmendieta <emendietaj@gmail.com>
2018-12-27 13:23:10 -0800

    Completed PointsDistance solution.

--- A_Java_Book/Chapter_3/PointsDistance.java
+++ A_Java_Book/Chapter_3/PointsDistance.java
@@ -5,7 +5,7 @@ public class PointsDistance {
    }
    
    public static double distance(int x1, int y1, int x2, int y2) {
-      
+      return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
    }
 
 }
