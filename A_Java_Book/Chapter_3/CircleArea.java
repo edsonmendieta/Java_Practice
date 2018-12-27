9b36fee407a6ef13b0d5c2f800074fd323eb04e3
edsonmendieta <emendietaj@gmail.com>
2018-12-27 13:06:45 -0800

    added new solution.

---
+++ A_Java_Book/Chapter_3/CircleArea.java
@@ -0,0 +1,14 @@
+public class CircleArea {
+
+   public static void main(String[] args) {
+      
+   }
+   
+   
+   public static double area(double radius) {
+   
+      return Math.PI * Math.pow(radius, 2);
+   
+   }
+
+}
