d69d767301dbdea6ae0cb7c1dbfedfedb7d035e2
edsonmendieta <emendietaj@gmail.com>
2018-12-27 13:30:45 -0800

    added new solution.

---
+++ A_Java_Book/Chapter_3/ScientificNotation.java
@@ -0,0 +1,11 @@
+public class ScientificNotation {
+
+   public static void main(String[] args) {
+   
+   }
+   
+   public static double scientific(double base, double exponent) {
+      return base * Math.pow(10, exponent);
+   }
+
+}
