317366becc266d3f912967dcb5afca34cc7978ad
edsonmendieta <emendietaj@gmail.com>
2018-12-27 12:49:52 -0800

    comleted Quadratic solution.

--- A_Java_Book/Chapter_3/Quadratic.java
+++ A_Java_Book/Chapter_3/Quadratic.java
@@ -1,13 +1,22 @@
 public class Quadratic {
+// method that takes the 3 coefficients in quadratic equation and outputs two roots (+ or -).
 
-   public class main(String[] args) {
+   public static void main(String[] args) {
+   
+   quadraticMethod(1, -7, 12);
+   quadraticMethod(1, 3, 2);
       
    }
    
    
-   public static double quadraticMethod(int a, int b, int c) {
-   
+   public static void quadraticMethod(int a, int b, int c) {
+
       
+      double positiveRoot = ((-1 * b) + Math.sqrt((Math.pow(b, 2) - (4 * a * c)))) / (2 * a);
+      double negativeRoot = ((-1 * b) - Math.sqrt((Math.pow(b, 2) - (4 * a * c)))) / (2 * a);
+      
+      System.out.println("First root = " + positiveRoot);
+      System.out.println("Second root = " + negativeRoot);
    
    } // quadraticMethod()
 
