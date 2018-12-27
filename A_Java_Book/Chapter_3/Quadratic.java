c210633c7250eb5fddce7d8cf3e81b5a65d6d807
edsonmendieta <emendietaj@gmail.com>
2018-12-27 12:32:45 -0800

    added pending solution.

---
+++ A_Java_Book/Chapter_3/Quadratic.java
@@ -0,0 +1,14 @@
+public class Quadratic {
+
+   public class main(String[] args) {
+      
+   }
+   
+   
+   public static double quadraticMethod(int a, int b, int c) {
+   
+      
+   
+   } // quadraticMethod()
+
+}
