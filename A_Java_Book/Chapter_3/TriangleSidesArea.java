a1712103a7a26bdc51993023d7404f217eb736a0
edsonmendieta <emendietaj@gmail.com>
2018-12-27 19:03:18 -0800

    completed solution.

--- A_Java_Book/Chapter_3/TriangleSidesArea.java
+++ A_Java_Book/Chapter_3/TriangleSidesArea.java
@@ -4,4 +4,9 @@ public class TriangleSidesArea {
    
    }
    
+   public static double triangleArea(double side1, double side2, double side3) {
+      double s = (side1 + side2 + side3) / 2.0;
+      return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+   }
+   
 }  
