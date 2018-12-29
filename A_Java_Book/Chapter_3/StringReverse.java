b988acfc1f5617980eb712f42e83c0159ee76651
edsonmendieta <emendietaj@gmail.com>
2018-12-29 10:12:09 -0800

    added clarifying comment.

--- A_Java_Book/Chapter_3/StringReverse.java
+++ A_Java_Book/Chapter_3/StringReverse.java
@@ -5,6 +5,7 @@ public class StringReverse {
    }
    
    public static void printReverse(String words) {
+   // prints a reversed version of the parameter string
       for (int i = words.length() - 1; i >= 0; i--) {
          System.out.print(words.charAt(i));
       }
