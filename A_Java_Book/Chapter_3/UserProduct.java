0db1c998f0163c8beb5b9ad0913a4579b3dbc63b
edsonmendieta <emendietaj@gmail.com>
2018-12-31 11:50:39 -0800

    added new solution.

---
+++ A_Java_Book/Chapter_3/UserProduct.java
@@ -0,0 +1,20 @@
+public class UserProduct {
+
+   public static void main(String[] arg) {
+      Scanner console = new Scanner(System.in);
+
+      System.out.print("How many numbers? ");
+      int totalNums = console.nextInt();
+
+      int product = 1;
+
+      for (int i = 1; i <= totalNums; i++) {
+         System.out.print("Next number --> ");
+         int userNum = console.nextInt();
+         product *= userNum;
+      }
+
+      System.out.print("Product = " + product);
+  }
+
+}
